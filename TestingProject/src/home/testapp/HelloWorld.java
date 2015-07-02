178de10c143bde02a3a7b0c9755145a2c7a430fd
Henry <henrychtang@gmail.com>
2015-07-03 00:18:23 +0800

    Some changes

---
+++ TestingProject/src/home/testapp/HelloWorld.java
@@ -0,0 +1,10 @@
+package home.testapp;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello My World!!!");
+	}
+
+}
